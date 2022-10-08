b90d556c18b04bde9b4462a4f4e3a02d94c4a2ea
IvanovaNA <IvanovaNA@310D-11>
2022-10-08 11:39:15 +0700

    Добавьте файлы проекта.

---
+++ Simple_factorial/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Numerics;
+
+namespace Simple_factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                static BigInteger SimpleFactorial(int x)
+                {
+                    var factorial = new BigInteger(1);
+                    for (int i = 1; i <= x; i++)
+                    {
+                        factorial *= i;
+                    }
+                    return factorial;
+                }
+
+                static BigInteger FactorialByRec(int x)
+                {
+                    if (x == 1) return 1;
+                    return x * FactorialByRec(x - 1);
+                }
+                
+            }
+           
+        }
+
+        
+
+    }
+}
